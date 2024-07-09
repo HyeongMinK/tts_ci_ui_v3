ddfdbedb3c965d0f77e1f31a9434394e2541a5ee
HyeongMinK <aalynm@gmail.com>
2024-07-09 18:23:27 +0900

    cash

--- app.py
+++ app.py
@@ -365,12 +365,14 @@ if __name__ == '__main__':
 
     if "process_started" not in st.session_state:
         st.session_state.process_started = False
+    if "text_file_uploaded" not in st.session_state:
+        st.session_state.text_file_uploaded = False
+    if "img_file_uploaded" not in st.session_state:
+        st.session_state.img_file_uploaded = False
 
     if not st.session_state.process_started:
         if st.button("영상 만들기 시작하기"):
-            # Streamlit 애플리케이션 시작 시 체크포인트 다운로드
             download_checkpoint()
-            # 다운로드 버튼이 눌리면 폴더 내의 모든 파일 삭제
             clear_directory("text_files")
             clear_directory("pic_files")
             clear_directory("results")
@@ -379,68 +381,49 @@ if __name__ == '__main__':
             st.rerun()
             
     if st.session_state.process_started:
-        api_key = os.getenv('OPENAI_API_KEY')  # 환경 변수에서 API 키를 가져옵니다.
+        api_key = os.getenv('OPENAI_API_KEY')
         if not api_key:
             raise ValueError("OPENAI_API_KEY 환경 변수가 설정되지 않았습니다.")
 
-        # 텍스트 파일 업로드 위젯 추가
-        uploaded_file = st.file_uploader("TTS 생성을 위한 텍스트 파일을 업로드 하세요", type="txt")
+        if not st.session_state.text_file_uploaded:
+            uploaded_file = st.file_uploader("TTS 생성을 위한 텍스트 파일을 업로드 하세요", type="txt")
 
-        if uploaded_file is not None:
-            # 업로드된 파일을 text_files 폴더에 저장
-            save_path = os.path.join("text_files", uploaded_file.name)
-            
-            # 디렉토리가 없으면 생성
-            if not os.path.exists("text_files"):
-                os.makedirs("text_files")
+            if uploaded_file is not None:
+                save_path = os.path.join("text_files", uploaded_file.name)
+                if not os.path.exists("text_files"):
+                    os.makedirs("text_files")
+                with open(save_path, "wb") as f:
+                    f.write(uploaded_file.getvalue())
+                with open(save_path, "r", encoding="utf-8") as f:
+                    file_contents = f.read()
+                    st.text_area("업로드된 텍스트 파일 내용", file_contents, height=150)
+                st.session_state.text_file_uploaded = True
 
-            # 파일 저장
-            with open(save_path, "wb") as f:
-                f.write(uploaded_file.getvalue())
-            
-            # 업로드된 텍스트 파일의 내용을 읽고 화면에 표시
-            with open(save_path, "r", encoding="utf-8") as f:
-                file_contents = f.read()
-                st.text_area("업로드된 텍스트 파일 내용", file_contents, height=150)
+        if not st.session_state.img_file_uploaded:
+            uploaded_img_file = st.file_uploader("이미지 파일을 업로드 하세요", type=["jpg", "jpeg", "png"])
 
-        # 이미지 파일 업로드 위젯 추가
-        uploaded_img_file = st.file_uploader("이미지 파일을 업로드 하세요", type=["jpg", "jpeg", "png"])
+            if uploaded_img_file is not None:
+                img_save_path = os.path.join("pic_files", uploaded_img_file.name)
+                if not os.path.exists("pic_files"):
+                    os.makedirs("pic_files")
+                with open(img_save_path, "wb") as f:
+                    f.write(uploaded_img_file.getvalue())
+                img = Image.open(img_save_path)
+                st.image(img, caption="업로드된 이미지", width=200)
+                st.session_state.img_file_uploaded = True
 
-        if uploaded_img_file is not None:
-            # 업로드된 파일을 pic_files 폴더에 저장
-            img_save_path = os.path.join("pic_files", uploaded_img_file.name)
-            
-            # 디렉토리가 없으면 생성
-            if not os.path.exists("pic_files"):
-                os.makedirs("pic_files")
-
-            # 파일 저장
-            with open(img_save_path, "wb") as f:
-                f.write(uploaded_img_file.getvalue())
-
-            # 업로드된 이미지 파일을 열고 화면에 표시
-            img = Image.open(img_save_path)
-            st.image(img, caption="업로드된 이미지", width=200)
-
-        if uploaded_file is not None and uploaded_img_file is not None:
-            # Streamlit 버튼을 추가하여 TTS 파일 생성 및 Wav2Lip 실행을 트리거
+        if st.session_state.text_file_uploaded and st.session_state.img_file_uploaded:
             if st.button("립싱크 영상 생성하기"):
                 with st.spinner("TTS 파일 생성 중..."):
-                    create_tts_files(api_key)  # TTS 파일 생성
-
+                    create_tts_files(api_key)
                 with st.spinner("영상 파일 생성 중..."):
-                    result_filename = main(img_save_path)  # Wav2Lip 실행 및 결과 파일 생성
-
-                # 결과 파일에 대해 다운로드 버튼 추가
+                    result_filename = main(img_save_path)
                 if os.path.exists(result_filename):
                     with open(result_filename, "rb") as f:
                         st.success("영상이 성공적으로 생성되었습니다.")
-                        if st.download_button(
-                            label=f"Download {os.path.basename(result_filename)}",
-                            data=f,
-                            file_name=os.path.basename(result_filename),
-                            mime="video/mp4"
-                        ):
-                            st.session_state.process_started = False
-                            st.rerun()
+                        st.download_button(label="다운로드", data=f, file_name="result_video.mp4")
+                        st.session_state.process_started = False
+                        st.session_state.text_file_uploaded = False
+                        st.session_state.img_file_uploaded = False
+                        st.rerun()
 
