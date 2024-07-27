8125468e939043fed1b80b34b1caeb36277c8068
HyeongMinK <aalynm@gmail.com>
2024-07-27 21:52:32 +0900

    v3

--- app.py
+++ app.py
@@ -344,7 +344,7 @@ def main(face_path):
         # 오디오 파일 이름을 기반으로 고유한 결과 파일 이름 생성
         audio_filename = os.path.splitext(os.path.basename(audio_file_path))[0]
         result_filename = f'results/result_voice_{audio_filename}.mov'
-        command = 'ffmpeg -y -i {} -i {} -strict -2 -q:v 1 {}'.format(audio_file_path, 'temp/result.avi', result_filename)
+         command = f'ffmpeg -y -i temp/result.avi -i {audio_file_path} -c:v copy -c:a aac -strict experimental {result_filename}'
         subprocess.call(command, shell=platform.system() != 'Windows')
 
         result_filenames.append(result_filename)
