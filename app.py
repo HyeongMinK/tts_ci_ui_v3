de2d917f4e14975c67e531eaead08cf8ecc9ffdf
HyeongMinK <aalynm@gmail.com>
2024-07-27 22:35:38 +0900

    v3

--- app.py
+++ app.py
@@ -347,7 +347,7 @@ def main(face_path):
             cv2.imwrite(f"{temp_dir}/frame_{idx:04d}.png", frame)
 
         # ffmpeg 명령어로 프레임을 비디오로 변환 (RGBA 지원)
-        output_video_path = 'temp/result.mov'
+        output_video_path = 'temp/result.avi'
         command = f'ffmpeg -y -framerate {fps} -i {temp_dir}/frame_%04d.png -s {frame_w}x{frame_h} -vf "format=rgba" -c:v libx264 -pix_fmt yuv420p {output_video_path}'
         subprocess.call(command, shell=True)
 
