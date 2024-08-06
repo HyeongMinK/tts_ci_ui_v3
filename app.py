82bae5c4fb68b2c3d2bcbe3d649927487ba6477e
HyeongMinK <aalynm@gmail.com>
2024-08-06 16:22:31 +0900

    v3

--- app.py
+++ app.py
@@ -348,13 +348,13 @@ def main(face_path):
             cv2.imwrite(f"{temp_dir}/frame_{idx:04d}.png", frame)
 
         # ffmpeg 명령어로 프레임을 비디오로 변환 (Apple ProRes 4444 코덱 사용)
-        output_video_path = 'temp/result.avi'
-        command = f'ffmpeg -y -framerate {fps} -i {temp_dir}/frame_%04d.png -s {frame_w}x{frame_h} -c:v ffv1 -pix_fmt rgba {output_video_path}'
+        output_video_path = 'temp/result.mov'
+        command = f'ffmpeg -y -framerate {fps} -i {temp_dir}/frame_%04d.png -s {frame_w}x{frame_h} -c:v prores_ks -profile:v 4444 -pix_fmt yuva444p10le {output_video_path}'
         subprocess.call(command, shell=True)
 
         # 오디오 파일 이름을 기반으로 고유한 결과 파일 이름 생성
         audio_filename = os.path.splitext(os.path.basename(audio_file_path))[0]
-        result_filename = f'results/result_voice_{audio_filename}.avi'
+        result_filename = f'results/result_voice_{audio_filename}.mov'
         command = f'ffmpeg -y -i {output_video_path} -i {audio_file_path} -c:v copy -c:a aac -strict experimental {result_filename}'
         subprocess.call(command, shell=platform.system() != 'Windows')
 
