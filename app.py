c5b6da0d3e0cc4a14e73ff9a81ceb5d34369672c
HyeongMinK <aalynm@gmail.com>
2024-08-10 01:11:30 +0900

    v3

--- app.py
+++ app.py
@@ -349,7 +349,8 @@ def main(face_path):
 
         # ffmpeg 명령어로 프레임을 비디오로 변환 (Apple ProRes 4444 코덱 사용)
         output_video_path = 'temp/result.mov'
-        command = f'ffmpeg -y -framerate {fps} -i {temp_dir}/frame_%04d.png -s {frame_w}x{frame_h} -c:v prores_ks -profile:v 4444 -pix_fmt yuva444p10le {output_video_path}'
+        command = f'ffmpeg -y -framerate {fps} -i {temp_dir}/frame_%04d.png -vf "geq=lum(A):a=\'min(255,max(0,(a*255/1023)))\'" -s {frame_w}x{frame_h} -c:v prores_ks -profile:v 4444 -pix_fmt yuva444p10le {output_video_path}'
+
         subprocess.call(command, shell=True)
 
         # 오디오 파일 이름을 기반으로 고유한 결과 파일 이름 생성
